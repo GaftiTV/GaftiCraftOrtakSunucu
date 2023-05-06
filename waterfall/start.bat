f3f4a6526d5f7692e6a915e40ea7d07fcf161fe4
okunamayanad <baran@okunamayanad.com>
2023-05-06 18:08:28 +0300

    Başlatma dosyalarını hazırladım

---
+++ waterfall/start.bat
@@ -0,0 +1,6 @@
+@REM 0.5 GB RAM
+
+@echo off
+title Waterfall
+cls
+java -Xms512M -Xmx512M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -jar velocity.jar
